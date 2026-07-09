595c539aea1628e1b473cc1d1bfa37be9c02006f
dionbalerr <dionbalerr@gmail.com>
2026-07-09 22:27:46 +0800

    + Initial recursion parsing done

--- app/src/main/java/com/dionbalerr/dhce/NfcCardReader.kt
+++ app/src/main/java/com/dionbalerr/dhce/NfcCardReader.kt
@@ -4,11 +4,13 @@ import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import android.util.Log
 import java.io.IOException
+import kotlin.experimental.and
 
 data class DiscoveredAid(val aid: String, val priority: Int)
 
 class NfcCardReader
 {
+    var masterList = mutableListOf<MutableMap<String, Any>>()
     fun readCard(tag: Tag): List<DiscoveredAid>
     {
         val isoDep = IsoDep.get(tag) ?: return emptyList()
@@ -23,9 +25,22 @@ class NfcCardReader
             printResponseApdu(responseBytes)
             if (StatusWord.matchSw(responseBytes).contentEquals(StatusWord.SUCCESS))
             {
-                return emptyList()
+                val byteWithoutSw = responseBytes.slice(0 until responseBytes.size - 2).toByteArray()
+                parseBerTlvTags(byteWithoutSw, 0, byteWithoutSw.size)
             }
             else return emptyList()
+
+            masterList.forEach()
+            {
+                val tagByte = (it["tag"] as ByteArray).toHexString().uppercase()
+                val length = it["length"]
+                val valueByte = (it["value"] as ByteArray).toHexString().uppercase()
+                Log.i("ByteTime",
+                    "Tag=${tagByte}, " +
+                            "Len=${length}, " +
+                            "Val=${valueByte}"
+                )
+            }
 
         }
         catch (e: IOException)
@@ -34,6 +49,48 @@ class NfcCardReader
         }
 
         return discoverableAids
+    }
+
+    private fun parseBerTlvTags(byteArray: ByteArray, startIndex: Int, endIndex: Int): MutableList<MutableMap<String, Any>>
+    {
+        var pointer = startIndex
+
+        while (pointer < endIndex)
+        {
+            val map = mutableMapOf<String, Any>()
+            val tagStartIndex = pointer
+
+            // Check tags here
+            val firstByteTag = byteArray[pointer]
+
+            if ((firstByteTag and 0x1F) == 0x1F.toByte())
+            {
+                pointer += 1
+            }
+            val tag = byteArray.sliceArray(tagStartIndex .. pointer)
+            map["tag"] = tag
+            pointer += 1
+
+            // Check length
+            val actualLength = byteArray[pointer].toInt() and 0xFF
+            map["length"] = actualLength
+            pointer += 1
+
+            // Check value
+            val value = byteArray.sliceArray(pointer until pointer + actualLength)
+            map["value"] = value
+            pointer += actualLength
+
+            masterList.add(map)
+
+            if ((tag[0].toInt() and 0x20) == 0x20)
+            {
+                parseBerTlvTags((map["value"] as ByteArray), 0, (map["value"] as ByteArray).size)
+            }
+
+        }
+
+        return masterList
     }
 
     fun printResponseApdu(byteArray: ByteArray)
